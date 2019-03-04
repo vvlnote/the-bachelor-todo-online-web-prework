9c78f33c0b40fb7af53e97ae9e53e3f868d26b2d
Joy Lin <vvlnote@hotmail.com>
2019-03-04 21:11:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -69,7 +69,7 @@ def get_average_age_for_season(data, season)
     end
   end
   if count != 0
-    average_age = total_age/count
+    average_age = total_age/count.to_f
   end
   average_age.round(0)
 end
