1eab6e16d5c718561ac5188d97e6cb0ea7733296
Joy Lin <vvlnote@hotmail.com>
2019-03-04 19:46:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,11 @@ def get_first_name_of_season_winner(data, season)
   # code here
   first_name = ""
   if data.has_key?(season)
-    data[season][]
+    name = data[season]["name"]
+    name_arr = name.split(" ")
+    first_name = name_arr[0]
   end
+  first_name
 end
 
 def get_contestant_name(data, occupation)
